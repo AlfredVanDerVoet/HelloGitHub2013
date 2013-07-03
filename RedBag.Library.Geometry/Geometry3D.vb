b81a2853552bde7c922a2e8d34203acf5b89959c
A. van der Voet <avdvoet@red-bag.com>
2013-07-03 21:36:22 +0200

    Spelling correction

--- RedBag.Library.Geometry/Geometry3D.vb
+++ RedBag.Library.Geometry/Geometry3D.vb
@@ -1,12 +1,11 @@
 ﻿Public MustInherit Class Geometry3D
 
     Public MustOverride Function OuterSurfaceArea() As Double
-    Public MustOverride Function InnterSurfaceArea() As Double
+    Public MustOverride Function InternalSurfaceArea() As Double
     Public MustOverride Function IsSolid() As Double
     Public MustOverride Function InnerVolumn() As Double
     Public MustOverride Function OuterVolumn() As Double
-    Public MustOverride Function WeightUnitMaterial() As Double
-    Public MustOverride Function NormalVector() As Double()
+    Public MustOverride Function WeightForUnitMaterial() As Double
 
     Public MustOverride Function Internals() As Geometry3D()
 
